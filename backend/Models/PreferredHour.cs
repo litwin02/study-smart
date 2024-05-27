2864e21f69a7cc650eab3ead8b82a59391aec10d
Przemysław Okun <94188021+OPrzemek@users.noreply.github.com>
2024-05-27 16:57:57 +0200

    Dodano modele

---
+++ backend/Models/PreferredHour.cs
@@ -0,0 +1,15 @@
+﻿using StudyCalendar.Server.Models;
+
+namespace StudiaPraca.Models
+{
+    public class PreferredHour
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public int ChosenHourId { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public Student Student { get; set; }
+        public ChosenHour ChosenHour { get; set; }
+    }
+}
---
+++ backend/Models/StudentsLecture.cs
@@ -0,0 +1,14 @@
+﻿namespace StudiaPraca.Models
+{
+    public class StudentsLecture
+    {
+        public int LectureId { get; set; }
+        public int StudentId { get; set; }
+        public int LecturerId { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public Lecture Lecture { get; set; }
+        public Student Student { get; set; }
+        public Lecturer Lecturer { get; set; }
+    }
+}

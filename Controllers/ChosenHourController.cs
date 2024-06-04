53795d084ae3a30134fb37e3c0dd14278df860d2
N0r3b0 <wfedde@gmail.com>
2024-06-04 23:50:38 +0200

    StudentController - Successfully added Student to database

--- Controllers/ChosenHourController.cs
+++ Controllers/ChosenHourController.cs
@@ -1,98 +1,98 @@
-﻿using Microsoft.AspNetCore.Cors;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using StudiaPraca.Contexts;
-using StudyCalendar.Server.Models;
+﻿//using Microsoft.AspNetCore.Cors;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
+//using StudiaPraca.Contexts;
+//using StudyCalendar.Server.Models;
 
-namespace StudyCalendar.Server.Controllers
-{
-    [EnableCors]
-    [ApiController]
-    [Route("[controller]")]
-    public class ChosenHourController : ControllerBase
-    {
-        private readonly AppDbContext _context;
+//namespace StudyCalendar.Server.Controllers
+//{
+//    [EnableCors]
+//    [ApiController]
+//    [Route("[controller]")]
+//    public class ChosenHourController : ControllerBase
+//    {
+//        private readonly AppDbContext _context;
 
-        public ChosenHourController(AppDbContext context)
-        {
-            _context = context;
-        }
+//        public ChosenHourController(AppDbContext context)
+//        {
+//            _context = context;
+//        }
 
-        // Metoda GET zwracająca wybrane godziny dla danego studenta
-        [HttpGet("student/{studentId:int}")]
-        public async Task<ActionResult<IEnumerable<ChosenHour>>> GetChosenHoursByStudent(int studentId)
-        {
-            var preferredHours = await _context.PreferredHours
-                .Where(ph => ph.StudentId == studentId)
-                .Select(ph => ph.ChosenHour)
-                .ToListAsync();
+//        // Metoda GET zwracająca wybrane godziny dla danego studenta
+//        [HttpGet("student/{studentId:int}")]
+//        public async Task<ActionResult<IEnumerable<ChosenHour>>> GetChosenHoursByStudent(int studentId)
+//        {
+//            var preferredHours = await _context.PreferredHours
+//                .Where(ph => ph.StudentId == studentId)
+//                .Select(ph => ph.ChosenHour)
+//                .ToListAsync();
 
-            if (preferredHours == null || !preferredHours.Any())
-            {
-                return NotFound();
-            }
+//            if (preferredHours == null || !preferredHours.Any())
+//            {
+//                return NotFound();
+//            }
 
-            return Ok(preferredHours);
-        }
+//            return Ok(preferredHours);
+//        }
 
-        [HttpGet("{id:int}")]
-        public async Task<ActionResult<ChosenHour>> GetChosenHour(int id)
-        {
-            var chosenHour = await _context.ChosenHours.FindAsync(id);
-            if (chosenHour == null)
-            {
-                return NotFound();
-            }
-            return chosenHour;
-        }
+//        [HttpGet("{id:int}")]
+//        public async Task<ActionResult<ChosenHour>> GetChosenHour(int id)
+//        {
+//            var chosenHour = await _context.ChosenHours.FindAsync(id);
+//            if (chosenHour == null)
+//            {
+//                return NotFound();
+//            }
+//            return chosenHour;
+//        }
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<ChosenHour>>> GetChosenHours()
-        {
-            return await _context.ChosenHours.ToListAsync();
-        }
+//        [HttpGet]
+//        public async Task<ActionResult<IEnumerable<ChosenHour>>> GetChosenHours()
+//        {
+//            return await _context.ChosenHours.ToListAsync();
+//        }
 
-        [HttpPut("add")]
-        public async Task<IActionResult> AddChosenHour([FromBody] Dictionary<string, int> requestData)
-        {
-            if (requestData.ContainsKey("day") && requestData.ContainsKey("hour"))
-            {
-                int day = requestData["day"];
-                int hour = requestData["hour"];
+//        [HttpPut("add")]
+//        public async Task<IActionResult> AddChosenHour([FromBody] Dictionary<string, int> requestData)
+//        {
+//            if (requestData.ContainsKey("day") && requestData.ContainsKey("hour"))
+//            {
+//                int day = requestData["day"];
+//                int hour = requestData["hour"];
 
-                var chosenHour = await _context.ChosenHours
-                    .FirstOrDefaultAsync(h => h.Day == day && h.Hour.Hour == hour);
+//                var chosenHour = await _context.ChosenHours
+//                    .FirstOrDefaultAsync(h => h.Day == day && h.Hour.Hour == hour);
 
-                if (chosenHour != null)
-                {
-                    chosenHour.Availability++;
-                    await _context.SaveChangesAsync();
-                    return Ok(chosenHour);
-                }
-            }
+//                if (chosenHour != null)
+//                {
+//                    chosenHour.Availability++;
+//                    await _context.SaveChangesAsync();
+//                    return Ok(chosenHour);
+//                }
+//            }
 
-            return BadRequest("Invalid data provided.");
-        }
+//            return BadRequest("Invalid data provided.");
+//        }
 
-        [HttpPut("remove")]
-        public async Task<IActionResult> RemoveChosenHour([FromBody] ChosenHour updatedHour)
-        {
-            var chosenHour = await _context.ChosenHours
-                .FirstOrDefaultAsync(h => h.Day == updatedHour.Day && h.Hour.Hour == updatedHour.Hour);
+//        [HttpPut("remove")]
+//        public async Task<IActionResult> RemoveChosenHour([FromBody] ChosenHour updatedHour)
+//        {
+//            var chosenHour = await _context.ChosenHours
+//                .FirstOrDefaultAsync(h => h.Day == updatedHour.Day && h.Hour.Hour == updatedHour.Hour);
 
-            if (chosenHour != null)
-            {
-                if (chosenHour.Availability == 0)
-                {
-                    return BadRequest();
-                }
+//            if (chosenHour != null)
+//            {
+//                if (chosenHour.Availability == 0)
+//                {
+//                    return BadRequest();
+//                }
 
-                chosenHour.Availability--;
-                await _context.SaveChangesAsync();
-                return Ok(chosenHour);
-            }
+//                chosenHour.Availability--;
+//                await _context.SaveChangesAsync();
+//                return Ok(chosenHour);
+//            }
 
-            return BadRequest();
-        }
-    }
-}
+//            return BadRequest();
+//        }
+//    }
+//}
---
+++ Migrations/20240604212626_InitialCreate.cs
@@ -0,0 +1,402 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace StudiaPraca.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChosenHours",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Day = table.Column<int>(type: "integer", nullable: false),
+                    Hour = table.Column<int>(type: "integer", nullable: false),
+                    Availability = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChosenHours", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lecturers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    IndexNumber = table.Column<string>(type: "text", nullable: false),
+                    FieldOfStudy = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lecturers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Planners",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Planners", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Capacity = table.Column<int>(type: "integer", nullable: false),
+                    Available = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Schedules",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedules", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    IndexNumber = table.Column<string>(type: "text", nullable: false),
+                    FieldOfStudy = table.Column<string>(type: "text", nullable: false),
+                    Year = table.Column<int>(type: "integer", nullable: false),
+                    Group = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subjects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Code = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subjects", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PreferredHours",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StudentId = table.Column<int>(type: "integer", nullable: false),
+                    ChosenHourId = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PreferredHours", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PreferredHours_ChosenHours_ChosenHourId",
+                        column: x => x.ChosenHourId,
+                        principalTable: "ChosenHours",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PreferredHours_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LecturerSubjects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    LecturerId = table.Column<int>(type: "integer", nullable: false),
+                    SubjectId = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LecturerSubjects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LecturerSubjects_Lecturers_LecturerId",
+                        column: x => x.LecturerId,
+                        principalTable: "Lecturers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LecturerSubjects_Subjects_SubjectId",
+                        column: x => x.SubjectId,
+                        principalTable: "Subjects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lectures",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SubjectId = table.Column<int>(type: "integer", nullable: false),
+                    ScheduleId = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lectures", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Lectures_Schedules_ScheduleId",
+                        column: x => x.ScheduleId,
+                        principalTable: "Schedules",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Lectures_Subjects_SubjectId",
+                        column: x => x.SubjectId,
+                        principalTable: "Subjects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ScheduledEvents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    SubjectId = table.Column<int>(type: "integer", nullable: false),
+                    LecturerId = table.Column<int>(type: "integer", nullable: false),
+                    RoomId = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ScheduleId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScheduledEvents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ScheduledEvents_Lecturers_LecturerId",
+                        column: x => x.LecturerId,
+                        principalTable: "Lecturers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ScheduledEvents_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ScheduledEvents_Schedules_ScheduleId",
+                        column: x => x.ScheduleId,
+                        principalTable: "Schedules",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ScheduledEvents_Subjects_SubjectId",
+                        column: x => x.SubjectId,
+                        principalTable: "Subjects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudentsLectures",
+                columns: table => new
+                {
+                    LectureId = table.Column<int>(type: "integer", nullable: false),
+                    StudentId = table.Column<int>(type: "integer", nullable: false),
+                    LecturerId = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentsLectures", x => new { x.LectureId, x.StudentId, x.LecturerId });
+                    table.ForeignKey(
+                        name: "FK_StudentsLectures_Lecturers_LecturerId",
+                        column: x => x.LecturerId,
+                        principalTable: "Lecturers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StudentsLectures_Lectures_LectureId",
+                        column: x => x.LectureId,
+                        principalTable: "Lectures",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StudentsLectures_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LecturerSubjects_LecturerId",
+                table: "LecturerSubjects",
+                column: "LecturerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LecturerSubjects_SubjectId",
+                table: "LecturerSubjects",
+                column: "SubjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lectures_ScheduleId",
+                table: "Lectures",
+                column: "ScheduleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lectures_SubjectId",
+                table: "Lectures",
+                column: "SubjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PreferredHours_ChosenHourId",
+                table: "PreferredHours",
+                column: "ChosenHourId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PreferredHours_StudentId",
+                table: "PreferredHours",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScheduledEvents_LecturerId",
+                table: "ScheduledEvents",
+                column: "LecturerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScheduledEvents_RoomId",
+                table: "ScheduledEvents",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScheduledEvents_ScheduleId",
+                table: "ScheduledEvents",
+                column: "ScheduleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScheduledEvents_SubjectId",
+                table: "ScheduledEvents",
+                column: "SubjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentsLectures_LecturerId",
+                table: "StudentsLectures",
+                column: "LecturerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentsLectures_StudentId",
+                table: "StudentsLectures",
+                column: "StudentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admins");
+
+            migrationBuilder.DropTable(
+                name: "LecturerSubjects");
+
+            migrationBuilder.DropTable(
+                name: "Planners");
+
+            migrationBuilder.DropTable(
+                name: "PreferredHours");
+
+            migrationBuilder.DropTable(
+                name: "ScheduledEvents");
+
+            migrationBuilder.DropTable(
+                name: "StudentsLectures");
+
+            migrationBuilder.DropTable(
+                name: "ChosenHours");
+
+            migrationBuilder.DropTable(
+                name: "Rooms");
+
+            migrationBuilder.DropTable(
+                name: "Lecturers");
+
+            migrationBuilder.DropTable(
+                name: "Lectures");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Schedules");
+
+            migrationBuilder.DropTable(
+                name: "Subjects");
+        }
+    }
+}

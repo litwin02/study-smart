2cf230ab8a6a3bd9c79944354d679b9534677c96
Oskru <oskrupaa@gmail.com>
2024-06-04 20:31:42 +0200

    refactor: Change project structure
    Added .gitignore file and removed backend/frontend directories. From now on, projects are kept within different repositories.

--- frontend/calendar/src/types/CalendarEvent.ts
+++
@@ -1,8 +0,0 @@
-export interface CalendarEvent {
-  day: number;
-  hour: number;
-  availability: number;
-
-  // id is sent from the server, but it's not required when creating a new event
-  id?: number;
-}
